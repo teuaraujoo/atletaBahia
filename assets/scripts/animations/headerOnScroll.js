d0037b69f8621c1a7b37dbae8560b589e36979cb
teuaraujoo <empresarial.mateus25@gmail.com>
2026-02-17 02:15:03 -0300

    responsividade

--- assets/scripts/animations/headerOnScroll.js
+++ assets/scripts/animations/headerOnScroll.js
@@ -3,11 +3,11 @@ export function headerOnScroll() {
 
     gsap.registerPlugin(ScrollTrigger);
 
-    ScrollTrigger.create({  
-    trigger: ".hero-wrapper",
-    start: "top top",
-    end: "bottom top",
-    onEnter: () => header.classList.add("scrolled"),
-    onLeaveBack: () => header.classList.remove("scrolled")
+    ScrollTrigger.create({
+        trigger: ".hero-wrapper",
+        start: "top top",
+        end: "bottom top",
+        onEnter: () => header.classList.add("scrolled"),
+        onLeaveBack: () => header.classList.remove("scrolled")
     });
 }
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,13 +5,15 @@ import { aboutAnimate } from "./animations/aboutGsap.js";
 import { highlightAnimate } from "./animations/highlightGsap.js";
 import { galleryAnimate } from "./animations/galleryGsap.js";
 import { contactAnimate } from "./animations/contactGsap.js";
+import { scrollSmooth } from "./animations/scrollSmooth.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     homeAnimate();
+    scrollSmooth();
+    headerOnScroll();
     aboutAnimate();
     highlightAnimate();
+    playVideo();
     galleryAnimate();
     contactAnimate();
-    headerOnScroll();
-    playVideo();
 })
